f6b97ae5e3c1ba6edd192ebbd4bdafb612918b80
John Pendexter <jpendexter@manifestcorp.com>
2017-05-07 11:00:19 -0400

    Adding a snake cased long test name for @DisplayName example.

--- src/test/java/com/pendext/junit/collections/ArrayListTest.java
+++ src/test/java/com/pendext/junit/collections/ArrayListTest.java
@@ -21,6 +21,13 @@ public class ArrayListTest {
     }
 
     @Test
+    public void adding_one_item_to_arrayList_shouldIncrement_sizeOfList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("one");
+        assertEquals(1, list.size());
+    }
+
+    @Test
     @DisplayName("Adding one item to an array list should increment the size of the list.")
     public void addingAnItemWorksWithDisplayName() {
         ArrayList<String> list = new ArrayList<>();

1cec7392626bd8d394cec80bc10c86950c11973d
John Pendexter <jpendexter@manifestcorp.com>
2017-05-20 15:41:58 -0400

    Assertions example

--- src/test/java/com/pendext/junit/spotify/SpotifyServiceIntegrationTest.java
+++ src/test/java/com/pendext/junit/spotify/SpotifyServiceIntegrationTest.java
@@ -1,13 +1,13 @@
 package com.pendext.junit.spotify;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.pendext.junit.annotations.NetworkIntegrationTest;
 import com.pendext.junit.spotify.transfer.*;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @NetworkIntegrationTest
 public class SpotifyServiceIntegrationTest {
@@ -21,7 +21,7 @@ public class SpotifyServiceIntegrationTest {
     }
 
     @Test
-    @DisplayName("Rest client can retrieve information from Spotify.")
+    @DisplayName("Spotify service can retrieve information from Spotify.")
     public void spotifyServiceCanRetrieveInformationFromSpotify() throws IOException {
         ArtistInfo artistInfo = spotifyService.getArtistInfo(ARTIST_ID);
         assertEquals(artistInfo.getName(), "Bill Monroe");
@@ -29,10 +29,36 @@ public class SpotifyServiceIntegrationTest {
     }
 
     @Test
+    @DisplayName("Spotify service returns valid top tracks list.")
     public void spotifyServiceCanReturnTopTracksList() throws IOException {
         TopTracksList topTracksList = spotifyService.getTopTracksList(ARTIST_ID);
-        assertNotNull(topTracksList);
-        assertEquals(10, topTracksList.getTracks().size());
+        assertNotNull(topTracksList, "The object was not and should not have been.");
+        assertEquals(10, topTracksList.getTracks().size(), "The number of actual top tracks did not match the expected.");
+    }
+
+    @Test
+    @DisplayName("Spotify service returns expected top tracks - using multiple asserts")
+    public void spotifyServiceReturnsExpectedTopTracksMultipleAsserts() throws IOException {
+        TopTracksList topTracksList = spotifyService.getTopTracksList(ARTIST_ID);
+        List<Track> tracks = topTracksList.getTracks();
+
+        assertEquals("Uncle Pen", tracks.get(0).getName(), "The track name was not what was expected.");
+        assertEquals("Southern Flavor", tracks.get(1).getName(), "The track name was not what was expected.");
+        assertEquals("Man of Constant Sorrow", tracks.get(2).getName(), "The track name was not what was expected.");
+        assertEquals("Pancho and Lefty", tracks.get(3).getName(), "The track name was not what was expected.");
+    }
+
+    @Test
+    @DisplayName("Spotify service returns expected top tracks - using assertAll")
+    public void spotifyServiceReturnsExpectedTopTracks() throws IOException {
+        TopTracksList topTracksList = spotifyService.getTopTracksList(ARTIST_ID);
+        List<Track> tracks = topTracksList.getTracks();
+
+        assertAll("Top tracks returned from Spotify are exactly the tracks expected.",
+                () -> assertEquals("Uncle Pen", tracks.get(0).getName(), "The track name was not what was expected."),
+                () -> assertEquals("Southern Flavor", tracks.get(1).getName(), "The track name was not what was expected."),
+                () -> assertEquals("Man of Constant Sorrow", tracks.get(2).getName(), "The track name was not what was expected."),
+                () -> assertEquals("Pancho and Lefty", tracks.get(3).getName(), "The track name was not what was expected."));
     }
 
 

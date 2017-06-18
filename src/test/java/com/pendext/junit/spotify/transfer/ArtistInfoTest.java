82da1bccc6394498d6ee9ba15799b19acb9259ba
John Pendexter <jpendexter@manifestcorp.com>
2017-06-18 11:53:52 -0400

    Putting the correct json back into the interface example, it had been removed for the code put into the talk

--- src/test/java/com/pendext/junit/spotify/transfer/ArtistInfoTest.java
+++ src/test/java/com/pendext/junit/spotify/transfer/ArtistInfoTest.java
@@ -6,9 +6,25 @@ import java.io.IOException;
 
 class ArtistInfoTest implements EqualsTestable<ArtistInfo> {
 
-    private String billMonroeJson = "{ ... }";
+    private String billMonroeJson = "{\n" +
+                                "\t\"genres\": [\"appalachian folk\", \"bluegrass\", \"country christmas\", \"country gospel\", \"cowboy western\", \"folk christmas\", \"nashville sound\", \"old-time\", \"progressive bluegrass\", \"traditional country\", \"traditional folk\"],\n" +
+                                "\t\"href\": \"https://api.spotify.com/v1/artists/5CWbfANRpZbnxdstzcNg5H\",\n" +
+                                "\t\"id\": \"5CWbfANRpZbnxdstzcNg5H\",\n" +
+                                "\t\"name\": \"Bill Monroe\",\n" +
+                                "\t\"popularity\": 35,\n" +
+                                "\t\"type\": \"artist\",\n" +
+                                "\t\"uri\": \"spotify:artist:5CWbfANRpZbnxdstzcNg5H\"\n" +
+                                "}";
 
-    private String otherArtistJson = "{ ... }";
+    private String otherArtistJson = "{\n" +
+                                "\t\"genres\": [\"bluegrass\", \"country\", \"country gospel\", \"cowboy western\", \"folk christmas\", \"heavy metal\", \"old-time\", \"progressive bluegrass\", \"traditional metal\"],\n" +
+                                "\t\"href\": \"https://api.spotify.com/v1/artists/1234fANRpZbnxdstzcNg5H\",\n" +
+                                "\t\"id\": \"1234fANRpZbnxdstzcNg5H\",\n" +
+                                "\t\"name\": \"Bill Monroe\",\n" +
+                                "\t\"popularity\": 42,\n" +
+                                "\t\"type\": \"artist\",\n" +
+                                "\t\"uri\": \"spotify:artist:1234fANRpZbnxdstzcNg5H\"\n" +
+                                "}";
 
     @Override
     public ArtistInfo createObject() throws IOException {

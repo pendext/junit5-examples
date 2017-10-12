b81604ab9af7f8a825e665b51c7caadc6ba69747
John Pendexter <jpendexter@manifestcorp.com>
2017-10-12 19:05:02 -0400

    Updating the version of JUnit to the most recent release version.

--- src/test/java/com/pendext/junit/util/EqualsTestable.java
+++ src/test/java/com/pendext/junit/util/EqualsTestable.java
@@ -1,7 +1,6 @@
 package com.pendext.junit.util;
 
-import static org.junit.gen5.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
